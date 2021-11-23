cffc8bc495354365bbe63182e918fa6350897c3a
Пудин Михаил Сергеевич <PMS.21@free.uni-dubna.ru>
2021-11-23 14:22:55 +0300

    Olimp

---
+++ Konteyner.py
@@ -0,0 +1,8 @@
+a = [i for i in range(0,11)]
+print(a[5])
+a[5]=100
+print(a)
+B = (i for i in range(0,11))
+print(b(5))
+B(5) = 100
+print(B)
---
+++ ovimpiada.py
@@ -0,0 +1,14 @@
+a = ()*3
+b = str(input())
+a.add(b)
+print(a)
+
+c = ()*3
+b = str(input())
+c.add(b)
+print(c)
+
+d = ()*3
+b = str(input())
+d.add(b)
+print(d)
