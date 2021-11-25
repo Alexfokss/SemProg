f9b848f9c03aaeb093f606cf17c11d3042b5c715
Alexfokss <94219318+Alexfokss@users.noreply.github.com>
2021-11-25 18:45:38 +0300

    Add files via upload

---
+++ Снежинка Коха.py
@@ -0,0 +1,25 @@
+import turtle 
+
+a, n =  map(int,input().split())
+def kokh(a, n):
+    if n == 0:
+        turtle.forward(a)
+    else:
+        kokh(a//3, n - 1)
+        turtle.left(60)
+        kokh(a//3, n - 1)
+        turtle.right(120)
+        kokh(a//3, n - 1)
+        turtle.left(60)
+        kokh(a//3, n - 1)
+
+
+turtle.shape("turtle")
+
+
+for i in range(n):
+    kokh(300, 3)
+    turtle.right(120)
+
+
+turtle.exitonclick()
