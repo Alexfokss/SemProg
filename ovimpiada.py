8cc3e62f7b5e8e3534f376ed7688ff0988760bc7
Alexfokss <monolit.pripyat@mail.ru>
2021-12-06 21:03:54 +0300

    olimpiada

--- ovimpiada.py
+++ ovimpiada.py
@@ -4,75 +4,97 @@
 # kol_balov -- количество баллов
 # unikal_nomer -- уникальный номер каждого участника
 # b -- имя участника
+# uchastnik_s_n_b -- имя участника, который в сумме по
+# трем олимпиадам набрал наибольше количество баллов. 										
+Olimpiada = dict()
 
-def olimpiada():
-    Olimpiada = dict()
+# def much_ballov():
+#     uchastnik_s_n_b = b
+#     kol_balov = p
+#     if > :
+#         sum_balov
 
-    def f1():
-        poryadkov_nomer = 0
-        b = ""
-        print("Введите количество участников 1 олипиады ")
-        h = int(input())
-        for i in range(h):
-            poryadkov_nomer += 1
-            print("Введите участника 1 олипиады ")
-            print("Введите количество набранных баллов ")
-            a = {}
-            b = str(input())
-            pervay_b = b
-            kol_balov = int(input)
-            a.add(b)
-            dlina = len(b)
-            print(a)
-            print()
-            unikal_nomer = f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}"
-            print("Уникальный номер", b, f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}")
-            a[unikal_nomer] = b
-            Olimpiada['1'] = a
+def f1():
+    poryadkov_nomer = 0
+    b = ""
+    print("Введите количество участников 1 олипиады ")
+    h = int(input())
+    for i in range(h):
+        poryadkov_nomer += 1
+        print("Введите участника 1 олипиады ")
+        print("Введите количество набранных баллов ")
+        b = str(input())
+        kol_balov = int(input())
+        pervay_b = b[0]
+        dlina = len(b)
+        print()
+        unikal_nomer = f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}"
+        print("Уникальный номер", b,": ", f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}")
+        print()
+        Olimpiada[unikal_nomer] = b
+    print()
+    print()
+    print(Olimpiada)
+    print()
+    print()
 
-    def f2():
-        poryadkov_nomer = 0
-        b = ""
-        print("Введите количество участников 2 олипиады ")
-        h = int(input())
-        for i in range(h):
-            poryadkov_nomer += 1
-            print("Введите участника 2 олипиады ")
-            print("Введите количество набранных баллов ")
-            c = {}
-            b = str(input())
-            pervay_b = b
-            kol_balov = int(input)
-            c.add(b)
-            dlina = len(b)
-            print(c)
-            print()
-            unikal_nomer = f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}"
-            print("Уникальный номер", b, f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}")
-            c[unikal_nomer] = b
-            Olimpiada['2'] = c
+def f2():
+    poryadkov_nomer = 0
+    b = ""
+    print("Введите количество участников 2 олипиады ")
+    h = int(input())
+    for i in range(h):
+        poryadkov_nomer += 1
+        print("Введите участника 2 олипиады ")
+        print("Введите количество набранных баллов ")
+        b = str(input())
+        kol_balov = int(input())
+        pervay_b = b[0]
+        dlina = len(b)
+        print()
+        unikal_nomer = f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}"
+        print("Уникальный номер", b,": ", f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}")
+        print()
+        Olimpiada[unikal_nomer] = b
+    print()
+    print()
+    print(Olimpiada)
+    print()
+    print()
 
 
-    def f3():
-        poryadkov_nomer = 0
-        b = ""
-        print("Введите количество участников 3 олипиады ")
-        h = int(input())
-        for i in range(h):
-            poryadkov_nomer += 1
-            print("Введите участника 3 олипиады ")
-            print("Введите количество набранных баллов ")
-            d = {}
-            b = str(input())
-            pervay_b = b
-            kol_balov = int(input)
-            d.add(b)
-            dlina = len(b)
-            print(d)
-            print()
-            unikal_nomer = f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}"
-            print("Уникальный номер", b, f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}")
-            d[unikal_nomer] = b
-            Olimpiada['3'] = d
+def f3():
+    poryadkov_nomer = 0
+    b = ""
+    print("Введите количество участников 3 олипиады ")
+    h = int(input())
+    for i in range(h):
+        poryadkov_nomer += 1
+        print("Введите участника 3 олипиады ")
+        print("Введите количество набранных баллов ")
+        b = str(input())
+        kol_balov = int(input())
+        pervay_b = b[0]
+        dlina = len(b)
+        print()
+        unikal_nomer = f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}"
+        print("Уникальный номер", b,": " , f"{poryadkov_nomer}_{pervay_b[0]}_{kol_balov}")
+        print()
+        Olimpiada[unikal_nomer] = b
+    print()
+    print()
+    print(Olimpiada)
+    print()
+    print()
 
-olimpiada()
+f1()
+f2()
+f3()
+
+
+
+
+#{'1_Л_34': 'Лисов Денис', '2_В_56': 'Вера Жукова', '1_А_87': 'Артём Полков', '2_С_34': 'Сергей Чашкин', '1_М_45': 'Маша Ролева', '2_П_29': 'Полина Крылова'} 
+
+
+
